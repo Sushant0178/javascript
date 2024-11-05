7b9bb49729cffd64dfee17fbfa6c3ac7327f94db
sushant kokate <91862793+Sushant0178@users.noreply.github.com>
2024-11-05 08:17:03 +0530

    .

---
+++ 1st.js
@@ -0,0 +1,8 @@
+// console.log("sushant")
+/*
+Ryan Dahl (born 1981) is an American software engineer who is best known for creating the Node. js JavaScript runtime as well as the Deno JavaScript/TypeScript runtime. Ryan Dahl. Dahl (2010; age 29).
+
+Brendan Eich. Brendan Eich (/ˈaɪk/ YK; born July 4, 1961) is an American computer programmer and technology executive. He created the JavaScript programming language and co-founded the Mozilla project, the Mozilla Foundation, and the Mozilla Corporation.
+
+*/
+
